b6aa55206eb1767f31e878c3a83ac7af9202af15
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-14 01:00:06 -0400

    Added menu based on links.

--- app/main/app.js
+++ app/main/app.js
@@ -1,23 +1,57 @@
-(function() {
+(function(that) {
   var spiteApp = angular.module("spiteApp", ["spiteAppControllers",
     "ngResource", "ngRoute"
   ]);
 
+  function MenuLinks(template, controller, link, text) {
+      this.template = template;
+      this.controller = controller;
+      this.link = link;
+      this.text = text;
+  };
+  that.MenuLinks = MenuLinks;
+
   var spiteAppControllers = angular.module("spiteAppControllers", []);
 
+  var links = [
+    new MenuLinks("/app/task-run/task-run-part.html",
+      "TaskRunCtrl", "/task-run", "Task Run"),
+    new MenuLinks("/app/task-add/task-add-part.html",
+      "TaskAddCtrl", "/task-add", "Task Add")
+  ];
+
+
+  spiteAppControllers.controller("MenuController", ["$scope", function($scope) {
+      $scope.headers = links;
+  }]);
+
   spiteApp.config(["$routeProvider", function($routeProvider) {
-    $routeProvider
-      .when("/task-run", {
-        templateUrl: "/app/task-run/task-run-part.html",
-        controller: "TaskRunCtrl"
-      })
-      .when("/task-add", {
-        templateUrl: "/app/task-add/task-add-part.html",
-        controller: "TaskAddCtrl"
-      })
-      .otherwise({
+    // $routeProvider
+    //   .when("/task-run", {
+    //     templateUrl: "/app/task-run/task-run-part.html",
+    //     controller: "TaskRunCtrl"
+    //   })
+    //   .when("/task-add", {
+    //     templateUrl: "/app/task-add/task-add-part.html",
+    //     controller: "TaskAddCtrl"
+    //   })
+    //   .otherwise({
+    //     redirectTo: "/"
+    //   });
+
+
+
+    var routeProvider = $routeProvider;
+    _(links).forEach(function(link) {
+        console.log("Setting links.");
+        routeProvider = routeProvider.when(link.link, {
+            templateUrl: link.template,
+            controller: link.controller
+        });
+    });
+    routeProvider.otherwise({
         redirectTo: "/"
-      });
+    });
 
   }]);
 
