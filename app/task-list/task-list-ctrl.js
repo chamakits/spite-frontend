b53abb60694eeda81ed0c911b345ff99afcf4b5a
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-15 01:19:30 -0400

    Began adding logic for running task.  Not nearly done.

--- app/task-list/task-list-ctrl.js
+++ app/task-list/task-list-ctrl.js
@@ -16,8 +16,21 @@
     });
   }]);
 
-  spiteAppControllers.controller("TaskListCtrl", ["$scope", "$q", "GetTasks",
-  function($scope, $q, GetTasks){
+  spiteAppControllers.factory("RunTaskNew", ["$resource", function($resource){
+    return $resource(URL, {}, {
+      query: {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        // isArray: true
+      }
+    });
+  }]);
+
+  spiteAppControllers.controller("TaskListCtrl", ["$scope", "$q", "GetTasks", "RunTaskNew",
+  function($scope, $q, GetTasks, RunTaskNew){
+
     var getTaskService = new GetTasks();
     var servicePromise = getTaskService.$save();
     servicePromise.then(
@@ -29,6 +42,14 @@
         console.log("Error:");
         console.log(err);
       });
+
+      $scope.runTask = function(task) {
+        console.log(task)
+        var runTaskService = new RunTaskNew();
+        // TODO rethink this.  Not doing it right.  Running task should probably go elsewhere
+        // TODO rethink how to get the data in.
+      };
+
   }]);
 
 }(this));
