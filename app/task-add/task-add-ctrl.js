b0c1d7a7f706ab2b53ddfc251a3f082f37f53067
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-17 02:07:02 -0400

    Added email purecss layout. Added when hovering over task a look change to it. Added toastr toast when submitting new task. Using now email layout in combination with menu layout.

--- app/task-add/task-add-ctrl.js
+++ app/task-add/task-add-ctrl.js
@@ -32,6 +32,22 @@
             }
 
             $scope.addTask = function(taskIn) {
+                toastr.options = {
+                    "closeButton": false,
+                    "debug": false,
+                    "positionClass": "toast-top-right",
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+
+                toastr.info('Task added!');
                 var toAddTask = new AddTask();
                 toAddTask.task = {
                     // "id" : taskIn.id,
