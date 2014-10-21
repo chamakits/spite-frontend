2d6c7d732d5a55b6c66a2a673ec561ed12e9d62f
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-21 01:34:22 -0400

    Changed how the controller works.

--- app/task-list/task-list-ctrl.js
+++ app/task-list/task-list-ctrl.js
@@ -28,8 +28,21 @@
         });
     }]);
 
-    spiteAppControllers.controller("TaskListCtrl", ["$scope", "$q", "GetTasks", "GetTaskDetail",
-        function($scope, $q, GetTasks, GetTaskDetail) {
+    var URLRunTask = "http://localhost:9090/api/run-task";
+    spiteAppControllers.factory("RunTask", ["$resource", function($resource) {
+        return $resource(URLRunTask, {}, {
+            query: {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        });
+    }]);
+
+    spiteAppControllers.controller("TaskListCtrl", [
+        "$scope", "$q", "GetTasks", "GetTaskDetail", "RunTask",
+        function($scope, $q, GetTasks, GetTaskDetail, RunTask) {
             // document.getElementById('layout').id = 'email-layout';
             ToEmailLayout();
             var getTaskService = new GetTasks();
@@ -75,7 +88,66 @@
                 // TODO rethink how to get the data in.
             };
 
+            $scope.runTask = function(taskDetail) {
+                //$scope.taskDetail.nameToTypeAndValue
+                var runTask = new RunTask();
+                runTask.view = {
+                    // "id" : taskIn.id,
+                    "name": taskDetail.name,
+                    "description": taskDetail.description,
+                    "nameToData": {}
+                };
+                runTask.data = {
+                    nameToData: {},
+                };
+                _.forEach(taskDetail.nameToTypeAndValue, function(current) {
+                    runTask.data.nameToData[current.fieldName] = current.value;
+                });
+
+                var deferred = $q.defer();
+                var promise = getEncodedFileUpload(deferred).promise;
+                promise.then(function(base64EncodedFile) {
+                    runTask.data.nameToData._upload = base64EncodedFile;
+                    runTask.$save().then(function(succ) {
+                        console.log("Succesfully called poast");
+                        console.log(succ);
+                    }, function(err) {
+                        console.log("Errored out in post call")
+                        console.log(err);
+                    });
+                }, function(err) {
+                    console.log("Errored out on encoding file");
+                    console.log(err);
+                });
+
+
+            };
+
         }
     ]);
 
+
+    function getEncodedFileUpload(deferred) {
+        var selectedFile = document.getElementById('fileInput').files[0];
+        if (!selectedFile) {
+            deferred.resolve(null);
+            return deferred;
+        }
+        var reader = new FileReader();
+
+
+        reader.onload = (function(promise) {
+            return function(e) {
+                var stringResult = e.target.result;
+                // console.log("Lenght of encoded string:"+stringResult.length)
+                console.log("Encoded file:");
+                console.log(stringResult);
+                promise.resolve(stringResult);
+            }
+        }(deferred));
+
+        reader.readAsDataURL(selectedFile);
+
+        return deferred;
+    }
 }(this));
